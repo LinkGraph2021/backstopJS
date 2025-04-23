f9005dbc5c02ebe93ae8cc5ad52d6b0bc587eae8
Judagutor <judagutor@gmail.com>
2025-04-23 11:26:52 -0500

    Add Puppeteer no-sandbox flags to Backstop config

--- backstop.config.template.js
+++ backstop.config.template.js
@@ -1,27 +1,33 @@
+// backstop.config.template.js
+
 module.exports = {
-  id: "dynamic-test",
+  id: 'visual_regression',
   viewports: [
-    { label: "mobile", width: 375, height: 812 },
-    { label: "tablet", width: 768, height: 1024 },
-    { label: "desktop", width: 1440, height: 900 }
-  ],
-  scenarios: [
     {
-      label: "Dynamic Test",
-      url: "{{URL}}",
-      selectors: ["{{SELECTOR}}"],
-      selectorExpansion: true,
-      delay: 500
+      label: 'desktop',
+      width: 1280,
+      height: 800,
+    },
+    {
+      label: 'mobile',
+      width: 375,
+      height: 812,
     }
   ],
+  scenarios: [
+    // This array will be overwritten dynamically by generate-config.js
+  ],
   paths: {
-    bitmaps_reference: "backstop_data/bitmaps_reference",
-    bitmaps_test: "backstop_data/bitmaps_test",
-    engine_scripts: "backstop_data/engine_scripts",
-    html_report: "backstop_data/html_report",
-    ci_report: "backstop_data/ci_report"
+    bitmaps_reference: 'backstop_data/bitmaps_reference',
+    bitmaps_test: 'backstop_data/bitmaps_test',
+    engine_scripts: 'backstop_data/engine_scripts',
+    html_report: 'backstop_data/html_report',
+    ci_report: 'backstop_data/ci_report'
   },
-  engine: "puppeteer",
-  report: ["browser"],
+  engine: 'puppeteer',
+  engineOptions: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  },
+  report: ['browser'],
   debug: false
 };
